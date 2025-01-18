b8fef81a8eaf93134159ac0001f1b097688fd55d
João Pedro Betanza Dal Caro <31898120+jpbetanza@users.noreply.github.com>
2025-01-18 14:59:58 +0000

    retornando branch main ao que estava antes

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,7 @@
 function Home(){
     return (
         <div className="flex flex-col gap-3">
-            <div>
-                <p>Vejo muitas declarações de amor, haha!</p>
-                <p>Mas, eu não tenho palavras de amor minhas para escrever...</p>
-                <p>Bem... sendo esse o caso, que não seja o meu amor,</p>
-                <p>Mas o de Cristo, que é o verdadeiro Amor encarnado.</p>
-            </div>
-            <br/>
-            <div>
-                <p>Aquele que é paciente, amoroso e manso... </p>
-                <p>Tentando todos os dias converter os corações endurecidos,</p>
-                <p>Dos assassinos, adúlteros, ladrões, miseráveis, viciados em drogas, estupradores.</p>
-                <p>Dos que buscam incansávelmente riquezas materiais.</p>
-                <p>Dos mentirosos, soberbos, egocentricos, raivosos, invejosos.</p>
-                <p>Dos que se deixam levar pelos vícios sexuais, preguiça e gula.</p>
-                <p>Dos que desonram seus pais. Dos que falam mal dos outros pelas costas.</p>
-                <p>Dos que se esqueceram de Deus, se esqueceram da Vida, do que realmente importa.</p>
-            </div>
-            <br/>
-            <div>
-                <p>Desses, Jesus é misericordioso todos os dias.</p>
-                <p>Todos os dias, Ele fala.</p>
-                <p>Todos os dias, Ele insiste.</p>
-                <p>Todos os dias, Ele suplica para que entendam</p>
-                <p>Que não importa o quanto busquem felicidades do mundo,</p>
-                <p>O vazio dos corações dos homens é infinito.</p>
-                <p>Não pode ser preenchido, senão pelo infinito amor de Deus.</p>
-                <p>"Convertei-vos e crede no evangelho"</p>
-            </div>
-            <br/>
-            <div>
-                <p>"Pai, perdoa-lhes, porque não sabem o que fazem"</p>
-                <p>Disse o Deus crucificado, esperando a última gota de sangue ser drenada de Seu corpo,</p>
-                <p>Enquanto espera ansiosamente de nós, ao menos uma gota de arrependimento,</p>
-                <p>Para que sejamos dignos de estar junto à Ele no paraíso, durante toda a eternidade.</p>
-            </div>
+            <h1>Atumalaca</h1>
          </div>
         )
 }
