9dfb2720a941e4cbb764b89f1894d1aef19aa39d
João Pedro Betanza Dal Caro <31898120+jpbetanza@users.noreply.github.com>
2025-01-23 00:35:06 +0000

    Adiciona scripts 'lint:check' e 'lint:fix'

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-function Home(){
-    return (
-        <div className="flex flex-col gap-3">
-            <h1>Atumalaca</h1>
-         </div>
-        )
+function Home() {
+  return (
+    <div className="flex flex-col gap-3">
+      <h1>Atumalaca</h1>
+    </div>
+  );
 }
-export default Home
+export default Home;
