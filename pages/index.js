7b635f7bceac6aeffb3e7a469e61f67c4b486464
João Pedro Betanza Dal Caro <31898120+jpbetanza@users.noreply.github.com>
2025-01-25 18:51:22 +0000

    Tela 'em construção criada'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 function Home() {
   return (
-    <div className="flex justify-center gap-3">
-      <h1>Atumalaca</h1>
+    <div className="h-screen flex flex-col justify-center items-center gap-3 p-4">
+      <Image src={"/images/blessedVirgin.png"} width={400} height={400} />
+      <h1 className="font-tangerine text-center text-6xl md:text-8xl">
+        Maria Imaculada
+      </h1>
+      <div className="flex flex-col gap-2 items-center text-center font-delius">
+        <p>Um site para prática da devoção à mãe de Deus!</p>
+        <p>
+          <b>Em construção...</b>
+        </p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      "tangerine": ["Tangerine", "serif"],
+      "delius": ["Delius", "serif"],
+    },
     extend: {},
   },
   variants: {
