1f988b33f8d5419c68844b8bde76b7958fc47e5b
João Pedro Betanza Dal Caro <31898120+jpbetanza@users.noreply.github.com>
2025-04-15 10:39:54 +0000

    finaliza endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Alunos do curso.dev são top" });
+  const updatedAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const dbName = process.env.POSTGRES_DB;
+  const connections = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;`,
+    values: [dbName],
+  });
+  console.log(connections);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        oppened_connections: connections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
+const { data } = require("autoprefixer");
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+  expect(parsedDate).toBe(responseBody.updated_at);
+  expect(responseBody.dependencies.database.version).toBe("16.6");
+  expect(responseBody.dependencies.database.oppened_connections).toBe(1);
 });
